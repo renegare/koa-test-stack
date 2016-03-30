07d00044a1c8445a8aa62492d9f9396fad6e6671
Mudi Ugbowanko <mudi@renegare.com>
2016-03-30 10:46:36 +0100

    initial test again

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var stacker = require('koa-compose')
 var co = require('co')
 
 module.exports = (stack, ctx) => {
-  return co(stacker(Array.isArray(stack)? stack : [stack]).bind(ctx))
+  return co(stacker(Array.isArray(stack) ? stack : [stack]).bind(ctx))
 }
--- test/run-middleware.js
+++ test/run-middleware.js
@@ -1,16 +1,19 @@
-var t = require('ava')
+var t = require('tap').test
+var pco = require('bluebird').coroutine
 var co = require('../')
 var expect = require('chai').expect
 var sinon = require('sinon')
 
-t('should execute handler', async t => {
+t('should execute handler', pco(function * (t) {
   var ctx = {}
   var stub = sinon.stub().returns('pong!')
 
-  await co(function * ping () {
+  yield co(function * ping () {
     this.body = stub()
   }, ctx)
 
   expect(ctx.body).to.equal('pong!')
   expect(stub.calledOnce).to.be.true
-})
+
+  t.end()
+}))
